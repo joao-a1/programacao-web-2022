d6bb9ad484d496a25aea0fa50d6842da03edc64e
Joao Arthur <joao.a1@aluno.ifsc.edu.br>
2022-04-16 16:13:20 -0300

    Autorização

---
+++ resources/views/altera_fornecedor.blade.php
@@ -0,0 +1,82 @@
+@extends('template')
+
+@section('titulo')
+Alterar fornecedor - #{{ $fornecedor->id }}
+@endsection
+
+@section('conteudo')
+<form method="post" action="{{ route('fornecedores_salvar') }}">
+        @csrf
+        <div class="card">
+            <div class="card-header bg-secondary mb-1 text-white">Alterar fornecedor - #{{ $fornecedor->id }}</div>
+            <div class="card mb-1 mx-auto" style="width: 99%;">
+                    <div class="card-header" style="margin-bottom: 16px;">Dados do fornecedor</div>
+                    <div class="container-fluid">
+                        <input type="hidden" name="id" value="{{ $fornecedor->id }}">
+                        <div class="input-group mb-3">
+                            <label class="input-group-text">Nome</label>
+                            <input id="nome" type="text" class="form-control" onchange="validar();" name="nome" value="{{ $fornecedor->nome }}">
+                        </div>
+                        <div class="input-group mb-3">
+                            <label class="input-group-text">Endereço</label>
+                            <input id="endereco" type="text" class="form-control" onchange="validar();" name="endereco" value="{{ $fornecedor->endereco }}">
+                        </div>
+                        <div class="input-group mb-3">
+                            <label class="input-group-text">CEP</label>
+                            <input id="cep" type="text" class="form-control" onchange="validar();" name="cep" value="{{ $fornecedor->cep }}">
+                        </div>
+                        <div class="input-group mb-3">
+                            <label class="input-group-text">Cidade</label>
+                            <input id="cidade" type="text" class="form-control" onchange="validar();" name="cidade" value="{{ $fornecedor->cidade }}">
+                        </div>
+                        <div class="input-group mb-3">
+                            <label class="input-group-text">Estado</label>
+                            <select id="estado" class="form-select" onchange="validar();" name="estado" value="{{ $fornecedor->estado }}">
+                                        <option value="">Selecione...</option>
+                                        <option value="AC">Acre</option>
+                                        <option value="AL">Alagoas</option>
+                                        <option value="AP">Amapá</option>
+                                        <option value="AM">Amazonas</option>
+                                        <option value="BA">Bahia</option>
+                                        <option value="CE">Ceará</option>
+                                        <option value="DF">Distrito Federal</option>
+                                        <option value="ES">Espírito Santo</option>
+                                        <option value="GO">Goiás</option>
+                                        <option value="MA">Maranhão</option>
+                                        <option value="MT">Mato Grosso</option>
+                                        <option value="MS">Mato Grosso do Sul</option>
+                                        <option value="MG">Minas Gerais</option>
+                                        <option value="PA">Pará</option>
+                                        <option value="PB">Paraíba</option>
+                                        <option value="PR">Paraná</option>
+                                        <option value="PE">Pernambuco</option>
+                                        <option value="PI">Piauí</option>
+                                        <option value="RJ">Rio de Janeiro</option>
+                                        <option value="RN">Rio Grande do Norte</option>
+                                        <option value="RS">Rio Grande do Sul</option>
+                                        <option value="RO">Rondônia</option>
+                                        <option value="RR">Roraima</option>
+                                        <option value="SC">Santa Catarina</option>
+                                        <option value="SP">São Paulo</option>
+                                        <option value="SE">Sergipe</option>
+                                        <option value="TO">Tocantins</option>
+                                        <option value="EX">Estrangeiro</option>
+                            </select>
+                        </div>
+                        <div class="input-group mb-3">
+                            <input id="salvar" type="submit" class="btn btn-success" value="salvar" disabled>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </form>
+
+<script>
+    function validar() {
+        if (nome.value && endereco.value && cep.value && cidade.value && estado.value)
+            salvar.disabled = false;
+        else
+            salvar.disabled = true;
+    }
+</script>
+@endsection
---
+++ resources/views/lista_cliente.blade.php
@@ -0,0 +1,40 @@
+@extends('template')
+
+@section('conteudo')
+
+<table class="table">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Telefone</th>
+            <th>Renda</th>
+            <th>Operações</th>
+        </tr>
+    </thead>
+    <tbody>
+    @foreach($clientes as $c)
+    <tr>
+        <td>{{ $c->id }}</td>
+        <td>{{ $c->nome }}</td>
+        <td>{{ $c->telefone }}</td>
+        <td>{{ $c->renda }}.00</td>
+        <td>
+            <a href="{{ route('clientes_alterar', ['id' => $c->id]) }}" class="btn btn-warning">Alterar</a>
+            <a href="{{ route('clientes_excluir', ['id' => $c->id]) }}" onclick="excluir({{ $c->id }})" class="btn btn-danger">Excluir</a>
+        </td>
+
+    </tr>
+    @endforeach
+    </tbody>
+</table>
+<a href="{{ route('clientes_novo')}}"><button class="btn btn-primary">Voltar</button> </a>
+
+<script>
+    function excluir(id){
+        if (confirm(`Deseja realmente excluir o cliente ${id}?`)){
+            location.hrfe = route('clientes_excluir', {'id':id});
+        }
+    }
+</script>
+@endsection

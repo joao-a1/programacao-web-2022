245fcedeaf068a770c9db13b439fd2523bd5672c
joao-a1 <73620157+joao-a1@users.noreply.github.com>
2022-04-16 15:56:07 -0300

    Add files via upload

---
+++ database/migrations/2022_04_10_210306_create_fornecedores.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fornecedores', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 200);
+            $table->string('endereco', 200);
+            $table->string('cep', 10);
+            $table->string('cidade', 100);
+            $table->string('estado', 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fornecedores');
+    }
+};
